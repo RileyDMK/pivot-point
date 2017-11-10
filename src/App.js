b05fc66b536e30368d571b7c8d88657e5b711bee
RileyDMK <rileydmathis@gmail.com>
2017-11-10 15:26:01 -0800

    Add Nav component

--- src/App.js
+++ src/App.js
@@ -6,15 +6,54 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <Nav></Nav>
+        {/* <header className="App-header">
+
+          {/* <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Pivot Point</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </header> */}
+        <div className="App-intro">
+          <h2>The Way Ahead is Just a PivotPoint away</h2>
+        </div>
+        <img src={require('./images/mtnpath.jpg')}/>
       </div>
     );
+  }
+}
+
+class Nav extends Component {
+  render(){
+    return (
+      <header>
+        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+          <a className="navbar-brand" href="#">Dashboard</a>
+          <button className="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Settings</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Profile</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Help</a>
+              </li>
+            </ul>
+            <form className="form-inline mt-2 mt-md-0">
+              <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"></input>
+              <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+          </div>
+        </nav>
+      </header>
+      );
   }
 }
 
